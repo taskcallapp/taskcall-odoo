7d011abbd30c8410ebee92fa77d5c669a635875b
riasatullah <riasat.ullah@gmail.com>
2021-06-23 10:23:52 +0200

    initial commit

---
+++ taskcall-odoo-helpdesk/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'TaskCall',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'TaskCall Cloud Services, S.L.',
+    'website': 'https://www.taskcallapp.com',
+    'category': 'Category',
+    'description': 'Productivity',
+    # # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
